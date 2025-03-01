cb507ccba0171f3c63bc6b764342200fa0639353
homeira kalim <homeirakalim@gmail.com>
2025-03-01 00:25:58 -0500

    react hooks jsx lab complete

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  console.log(image);
+  return (
+    <div id="about" style={{ textAlign: "center"}}>
+      <h2 style= {{fontSize: "40px"}}>About Me</h2>
+      <p>I like ice cream</p>
+      <img src={image} alt="I made this"></img>
+    </div>
+  ); 
 }
 
 export default About;
